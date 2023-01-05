ca3595036dfcb56d86625504a79994c09d973e1e
d-perado <sjm01065@naver.com>
2023-01-05 12:06:03 +0900

    [level 3] Title: 여행경로, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -0,0 +1,29 @@
+function solution(tickets) {
+    var answer = [];
+    const result=[];
+    const len= tickets.length
+    const visited=[];
+    tickets.sort();
+    
+    const dfs=(str,count)=>{
+        result.push(str);
+        
+        if(count===len){
+            answer=result
+            return true;
+        }
+            for(let i=0;i<len;i++){
+        if(!visited[i]&&tickets[i][0]===str){
+            visited[i]=true;
+            if(dfs(tickets[i][1],count+1))return true;
+            visited[i]=false;
+        }
+    }
+    result.pop();
+    
+    return false;
+    }
+    
+    dfs("ICN",0);
+    return answer;
+}
