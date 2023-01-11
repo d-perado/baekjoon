f87db884aba509c6dfa86033f6e42a44f8213e7b
d-perado <sjm01065@naver.com>
2023-01-11 17:21:18 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 232 ms, Memory: 15424 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.js
@@ -0,0 +1,76 @@
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  size = () => {
+    return this.heap.length;
+  };
+
+  push = (node) => {
+    this.heap.push(node);
+    let i = this.heap.length - 1;
+    let parentI = Math.floor((i - 1) / 2);
+    while (i > 0 && this.heap[parentI] > this.heap[i]) {
+      this.swap(i, parentI);
+      i = parentI;
+      parentI = Math.floor((i - 1) / 2);
+    }
+  };
+
+  pop = () => {
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
+    const result = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    let i = 0;
+    while (true) {
+      const leftI = i * 2 + 1,
+        rightI = i * 2 + 2;
+      if (leftI >= this.heap.size) {
+        break;
+      }
+      let nextI = i;
+      if (this.heap[nextI] > this.heap[leftI]) {
+        nextI = leftI;
+      }
+      if (rightI < this.heap.length && this.heap[nextI] > this.heap[rightI]) {
+        nextI = rightI;
+      }
+      if (nextI === i) {
+        break;
+      }
+      this.swap(i, nextI);
+      i = nextI;
+    }
+    return result;
+  };
+
+  swap = (a, b) => {
+    const temp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = temp;
+  };
+}
+
+let n = +input[0];
+let pq = new MinHeap();
+for (let i = 1; i <= n; i++){
+  pq.push(+input[i]);
+}
+let result = +0;
+while (pq.size() > 1) {
+  const first = pq.pop();
+  const second = pq.pop();
+  result += first + second;
+  pq.push(first + second);
+}
+console.log(result);
