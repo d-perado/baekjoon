73f8cd109725fc0ad588d7f0c58cc73e90e8a4e2
d-perado <sjm01065@naver.com>
2023-01-12 13:35:25 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    var answer = [];
+    for(let i=0;i<commands.length;i++){
+        const k = array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b)
+        answer.push(k[commands[i][2]-1])
+    }
+    
+    return answer;
+}
