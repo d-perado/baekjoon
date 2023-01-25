a6989ecbfefc2202629e4702f1632dd848dfe582
d-perado <sjm01065@naver.com>
2023-01-25 14:57:16 +0900

    [Gold I] Title: 타일 채우기, Time: 232 ms, Memory: 10664 KB -BaekjoonHub

---
+++ 백준/Gold/2718. 타일 채우기/타일 채우기.js
@@ -0,0 +1,25 @@
+const fs = require("fs")
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt"
+const [T, ...input] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number)
+
+const dp = new Array(30).fill(0)
+dp[0] = 1
+dp[1] = 5
+dp[2] = 11
+dp[3] = 36
+
+let cnt = 5
+for (let i = 0; i < T; i++) {
+  if (dp[input[i] - 1] === 0) {
+    for (let j = cnt - 1; j < input[i]; j++) {
+      dp[j] = dp[j - 1] + dp[j - 2] * 5 + dp[j - 3] - dp[j - 4]
+    }
+  }
+  console.log(dp[input[i] - 1])
+  cnt = Math.max(cnt, input[i])
+}
