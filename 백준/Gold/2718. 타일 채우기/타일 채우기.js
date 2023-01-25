8d470d4bdeb9436b0209d47d1f2f48f68e46fdb8
d-perado <sjm01065@naver.com>
2023-01-25 15:11:54 +0900

    [Gold I] Title: 타일 채우기, Time: 164 ms, Memory: 10672 KB -BaekjoonHub

--- 백준/Gold/2718. 타일 채우기/타일 채우기.js
+++ 백준/Gold/2718. 타일 채우기/타일 채우기.js
@@ -7,7 +7,7 @@ const [T, ...input] = fs
   .split("\n")
   .map(Number)
 
-const dp = new Array(30).fill(0)
+const dp = new Array(500).fill(0)
 dp[0] = 1
 dp[1] = 5
 dp[2] = 11
@@ -18,6 +18,9 @@ for (let i = 0; i < T; i++) {
   if (dp[input[i] - 1] === 0) {
     for (let j = cnt - 1; j < input[i]; j++) {
       dp[j] = dp[j - 1] + dp[j - 2] * 5 + dp[j - 3] - dp[j - 4]
+
+      //j-1 << 앞의 타일 배치 수와 동일 앞의타일배치 + 일자로
+      //j-2 << 두개타일로 배치될 수있는 가짓 수 중
     }
   }
   console.log(dp[input[i] - 1])
