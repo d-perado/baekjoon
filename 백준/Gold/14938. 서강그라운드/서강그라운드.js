b66881cd8a37d06fd18d070f9b33debcbfaa67a9
d-perado <sjm01065@naver.com>
2023-02-05 17:03:05 +0900

    [Gold IV] Title: 서강그라운드, Time: 196 ms, Memory: 11812 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.js
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.js
@@ -1,113 +1,36 @@
-'use strict';
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-const readline = require('readline');
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
-
-class QElement {
-	constructor(element, priority) {
-		this.element = element;
-		this.priority = priority;
-	}
+const [n, m, r] = input[0].split(" ").map((x) => +x);
+const reward = input[1].split(" ").map((x) => +x);
+//console.log(reward);
+const board = Array.from({ length: n + 1 }, () =>
+  new Array(n + 1).fill(Infinity)
+);
+for (let i = 1; i <= n; i++) {
+  board[i][i] = 0;
 }
 
-class PriorityQueue {
-	constructor() {
-		this.queue = [];
-	}
-	enqueue(element, priority) {
-		let isContain = false;
-		const qElement = new QElement(element, priority);
-		for (let i = 0; i < this.queue.length; i++) {
-			if (this.queue[i].priority > qElement.priority) {
-				this.queue.splice(i, 0, qElement);
-				isContain = true;
-				break;
-			}
-		}
-		if (!isContain) {
-			this.queue.push(qElement);
-		}
-	}
-	dequeue() {
-		if (!this.isEmpty()) return this.queue.shift();
-	}
-	front() {
-		if (!this.isEmpty()) return this.queue[0];
-	}
-	rear() {
-		if (!this.isEmpty()) return this.queue[this.queue.length - 1];
-	}
-	isEmpty() {
-		return this.queue.length === 0;
-	}
+for (let i = 2; i < 2 + r; i++) {
+  const [a, b, w] = input[i].split(" ").map((x) => +x);
+  board[a][b] = w;
+  board[b][a] = w;
 }
 
-const dijkstra = (startPoint, items, edges, n, m) => {
-	const distance = Array.from({ length: n + 1 }, () => Infinity);
-	// const visited = Array.from({ length: n + 1 }, () => 0);
-	const pq = new PriorityQueue();
-
-	// visited[startPoint] = 0;
-	distance[startPoint] = 0;
-	pq.enqueue(startPoint, 0);
-
-	while (!pq.isEmpty()) {
-		const qElement = pq.dequeue();
-		for (const edge of edges[qElement.element]) {
-			const [d, w] = edge;
-			// if (!visited[d]) {
-			if (distance[qElement.element] + w < distance[d]) {
-				distance[d] = distance[qElement.element] + w;
-				pq.enqueue(d, distance[d]);
-				// visited[d] = 1;
-			}
-			// }
-		}
-	}
-	let itemCount = 0;
-	for (let i = 1; i < n + 1; i++) {
-		// 수색범위 안이면
-		if (distance[i] <= m) {
-			itemCount += items[i - 1];
-		}
-	}
-	return itemCount;
-};
-
-const solution = function (input) {
-	const [n, m, r] = input
-		.shift()
-		.split(' ')
-		.map((e) => parseInt(e));
-	const items = input
-		.shift()
-		.split(' ')
-		.map((e) => parseInt(e));
-	const edges = {};
-	for (let i = 1; i < n + 1; i++) {
-		edges[i] = [];
-	}
-	for (const row of input) {
-		const [a, b, c] = row.split(' ').map((e) => parseInt(e));
-		edges[a].push([b, c]);
-		edges[b].push([a, c]);
-	}
-
-	let maxitems = 0;
-	for (let startPoint = 1; startPoint < n + 1; startPoint++) {
-		maxitems = Math.max(maxitems, dijkstra(startPoint, items, edges, n, m));
-	}
-
-	console.log(maxitems);
-};
-
-const input = [];
-rl.on('line', function (line) {
-	input.push(line);
-}).on('close', function () {
-	solution(input);
-	process.exit();
-});
+for (let k = 1; k <= n; k++) {
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      board[i][j] = Math.min(board[i][j], board[i][k] + board[k][j]);
+    }
+  }
+}
+let max = 0;
+for (let i = 1; i <= n; i++) {
+  let count = 0;
+  for (let j = 1; j <= n; j++) {
+    if (board[i][j] <= m) count += reward[j - 1];
+  }
+  max = Math.max(max, count);
+}
+//console.log(board);
+console.log(max);
