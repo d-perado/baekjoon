ef9d54f04368d79b299d9ee3e4e17c9ed85792ef
d-perado <sjm01065@naver.com>
2025-10-16 16:36:58 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        boolean[] checked = new boolean[10];
+        
+        for(int i=0;i<numbers.length;i++){
+            checked[numbers[i]]=true;
+        }
+        for(int i=1;i<checked.length;i++){
+            if(!checked[i]){
+                answer+=i;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
