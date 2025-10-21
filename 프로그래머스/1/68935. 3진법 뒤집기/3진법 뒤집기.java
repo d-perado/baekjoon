358ecf4ab4b5107bad9efaf76694be7144db6b74
d-perado <sjm01065@naver.com>
2025-10-21 10:10:54 +0900

    [level 1] Title: 3진법 뒤집기, Time: 17.77 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        String str = "";
+
+        while (n != 0) {
+            str += n % 3;
+            n /= 3;
+        }
+        
+        return Integer.parseInt(str, 3);
+    }
+}
