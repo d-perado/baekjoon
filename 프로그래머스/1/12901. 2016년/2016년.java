c11ca0701f05fcd74b0e2c331e4f39a79361da37
d-perado <sjm01065@naver.com>
2025-10-27 09:33:07 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        
+        String[] WEEK = {"FRI", "SAT", "SUN", "MON","TUE", "WED", "THU"};
+        
+        int[] DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        int monthDiff = a - 1;
+        
+        int dayDiff = b - 1;
+        for(int i = 0; i < monthDiff; i++){
+            dayDiff += DAYS[i];
+        }
+        dayDiff %= 7;
+        answer = WEEK[dayDiff];
+        
+        
+        return answer;
+    }
+}
