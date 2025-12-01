875da5670d378e580705973e98d4536e8c63bcf7
d-perado <sjm01065@naver.com>
2025-12-01 09:55:54 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 63.00 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,18 +2,21 @@ class Solution {
     public String solution(int[] food) {
         String answer = "";
         
-        for(int i=1;i<food.length;i++){
-            while(food[i]>1){
+        for(int i = 1; i < food.length; i++){
+            while(food[i] > 1){
                 answer += i;
-                food[i] -= 2;
+                food[i] -= 2; 
             }
         }
-        String temp = answer;
-        answer += 0;
-        for(int i = 0; i < temp.length();i++){
-            answer += temp.charAt(temp.length()-i-1);
+
+        
+        String temp = answer;  
+        answer += 0;         
+        
+        for(int i = 0; i < temp.length(); i++){
+            answer += temp.charAt(temp.length() - i - 1);
         }
         
-        return answer;
+        return answer; 
     }
 }
