6e05c33ca00785b791e4b2df76eb4ecb4c0203ac
d-perado <sjm01065@naver.com>
2025-12-02 09:48:45 +0900

    [level 1] Title: 콜라 문제, Time: 0.15 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -6,12 +6,12 @@ class Solution {
         int emptyCoke = 0;
         
         
-        while(emptyCoke + fullCoke >= a){
-            emptyCoke += fullCoke;
-            fullCoke = 0;
-            fullCoke += (emptyCoke/a)*b;            
+        while(emptyCoke + fullCoke >= a){ //콜라병의 총갯수가 a보다 작을 때 탈출
+            emptyCoke += fullCoke; //콜라를 마셔
+            fullCoke = 0; //콜라를 다 마셨어
+            fullCoke += (emptyCoke/a)*b;   //빈콜라를 새 콜라로바꿔줌          
             emptyCoke -= (emptyCoke/a)*a;
-            answer += fullCoke;
+            answer += fullCoke; // 새콜라를 값에 더해서 총 마실수 있는 콜라 갯수를 만들어줌.
         }
         
         return answer;
