1f14687e9180a4f91b56fda428cf1d83913e28de
d-perado <sjm01065@naver.com>
2025-12-04 09:44:26 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -7,11 +7,12 @@ class Solution {
         int[] DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         
         int monthDiff = a - 1;
-        
         int dayDiff = b - 1;
+        
         for(int i = 0; i < monthDiff; i++){
             dayDiff += DAYS[i];
         }
+        
         dayDiff %= 7;
         answer = WEEK[dayDiff];
         
