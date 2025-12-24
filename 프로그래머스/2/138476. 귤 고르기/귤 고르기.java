f8dc1e02f6044676c4c13556d64e420a90e4a90d
d-perado <sjm01065@naver.com>
2025-12-24 09:29:15 +0900

    [level 2] Title: 귤 고르기, Time: 174.10 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,33 +1,28 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
 
-        Map<Integer, Integer> countMap = new HashMap<>();
-        
+        int[] arr = new int[10000001];
+
+
         for (int size : tangerine) {
-            countMap.put(size, countMap.getOrDefault(size, 0) + 1);
+            arr[size]++;
         }
 
-        Integer[] counts = new Integer[countMap.size()];
-        int idx = 0;
-        for (int count : countMap.values()) {
-            counts[idx++] = count;
-        }
 
-        Arrays.sort(counts, (a, b) -> b - a);
+        Arrays.sort(arr);
 
-        for (int count : counts) {
-            k -= count;
-            answer++;
+        for (int i = 10000000; i >= 1; i--) {
+            if (arr[i] > 0) {
+                k -= arr[i];
+                answer++; 
+            }
             if (k <= 0) {
                 break;
             }
         }
-
         return answer;
     }
 }
