6687a082d84d9c6fb364e993014bd19afc141acc
d-perado <sjm01065@naver.com>
2025-12-26 10:25:53 +0900

    [level 2] Title: 괄호 회전하기, Time: 32.21 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,39 +1,6 @@
-import java.util.*;
-
 class Solution {
-    
-    private boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) return false;
-                
-                char top = stack.pop();
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
-                    return false;
-                }
-            }
-        }
-        
-        return stack.isEmpty();
-    }
-    
     public int solution(String s) {
-        int answer = 0;
-        int n = s.length();
-        
-        for (int i = 0; i < n; i++) {
-            String rotated = s.substring(i) + s.substring(0, i);
-            if (isValid(rotated)) {
-                answer++;
-            }
-        }
-        
+        int answer = -1;
         return answer;
     }
 }
